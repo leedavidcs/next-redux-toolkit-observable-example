1bcd7a6655d4a1dac33cccc8f19a3adc563f2568
leedavidcs <lee.david.cs@gmail.com>
2020-08-24 19:38:29 -0700

    Added types to api

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,6 +1,9 @@
+import { StatusCodes } from "http-status-codes";
+import { NextApiRequest, NextApiResponse } from "next";
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
-	res.statusCode = 200;
+export default (req: NextApiRequest, res: NextApiResponse) => {
+	res.statusCode = StatusCodes.OK;
 	res.json({ name: "John Doe" });
 };
